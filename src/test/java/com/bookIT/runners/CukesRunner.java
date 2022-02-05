dc0bc584dd1dc1e257bd421b6c701532702278b4
Taras Demianiv <taras.kor@gmail.com>
2022-02-05 14:36:16 -0800

    updated CukesRunner for getting Reports

--- src/test/java/com/bookIT/runners/CukesRunner.java
+++ src/test/java/com/bookIT/runners/CukesRunner.java
@@ -6,6 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt", // store failed scenario names into rerun.txt file
+                "json:target/cucumber.json" // generate json execution report to be used for html report
+        },
         features = "src/test/resources/features",
         glue = "com/bookIT/step_definition",
         dryRun = false,
